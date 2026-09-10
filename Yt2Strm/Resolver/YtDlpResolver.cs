0cb106ac0ecece6841b79b7961cf252b06336d41
Ahamed Zulfan <ahamedzulfan@gmail.com>
2026-09-10 16:13:27 +0400

    v1.3.6.0

--- Yt2Strm/Resolver/YtDlpResolver.cs
+++ Yt2Strm/Resolver/YtDlpResolver.cs
@@ -39,9 +39,8 @@ public class YtDlpResolver
     /// <summary>Resolve the direct stream URL, prioritizing MP4 formats before falling back.</summary>
     public (string streamUrl, string error) ResolveUrl(string url, string format)
     {
-        // 1. Try resolving strictly for MP4 first
-        var mp4FormatSelector = "mp4";
-        var args = string.Format(CultureInfo.InvariantCulture, "--js-runtimes node -g \"{0}\"", url);
+        var FormatSelector = "best/bestvideo+bestaudio";
+        var args = string.Format(CultureInfo.InvariantCulture, "--js-runtimes node -f \"{0}\" -g \"{1}\"", FormatSelector, url);
         var (stdout, stderr) = RunYtDlp(args);
 
         if (!string.IsNullOrEmpty(stdout))
